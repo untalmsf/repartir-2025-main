e36f801704a41ec3815593b7fac38a67f7eac107
Marco Saporiti <saporiti.marco.43792@gmail.com>
2025-06-14 16:28:26 -0300

    validar mail usuario step

---
+++ src/fastAcceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/UsuarioMailSteps.java
@@ -0,0 +1,27 @@
+package ar.com.grupoesfera.repartir.steps.grupos;
+
+
+import ar.com.grupoesfera.repartir.model.Usuario;
+import io.cucumber.java.es.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class UsuarioMailSteps {
+    private Usuario usuario;
+    private Exception error;
+
+    @Dado("que existe un usuario con id {string} y correo {string}")
+    public void existeUsuario(String id, String correo) {
+        usuario = new Usuario();
+        usuario.setId(id);
+        try {
+            usuario.setCorreo(correo);
+        } catch (Exception e) {
+            error = e;
+        }
+    }
+
+    @Cuando("consulto ese usuario")
+    public void consultoUsuario() {
+        // No hace nada: la validación ocurre en el setter
+    }
+}
